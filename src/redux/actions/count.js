27d5a905ce639583c3a3ea2ea3ccb0d46e87e41e
SuperTyler <896267775@qq.com>
2022-04-18 00:30:56 +0800

    no arrow-parens if only one param

--- src/redux/actions/count.js
+++ src/redux/actions/count.js
@@ -1,9 +1,9 @@
 import { INCREMENT, DECREMENT } from "../constant";
 
-export const increment = (data) => ({ type: INCREMENT, data });
-export const decrement = (data) => ({ type: DECREMENT, data });
+export const increment = data => ({ type: INCREMENT, data });
+export const decrement = data => ({ type: DECREMENT, data });
 
-export const incrementAsync = (data, time) => (dispatch) => {
+export const incrementAsync = (data, time) => dispatch => {
   setTimeout(() => {
     dispatch(increment(data));
   }, time);
